dd303ed650e1190b6a95507acbfe75934b447646
Danny Harding <dannyharding10@gmail.com>
2026-02-12 16:21:00 -0600

    feat: add minimap, autosession, keymaps for gitsigns, updated config for lualine

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,11 +26,53 @@ require("lazy").setup({
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = { options = { theme = "onedark", globalstatus = true } },
+		opts = {
+			options = { theme = "onedark", globalstatus = true },
+			sections = {
+				lualine_c = { { "filename", path = 1 } },
+			},
+		},
 	},
 
 	-- Git signs
-	{ "lewis6991/gitsigns.nvim", opts = {} },
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			on_attach = function(buffer)
+				local gs = require("gitsigns")
+				local function map(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					gs.nav_hunk("next")
+				end, "Next hunk")
+				map("n", "[c", function()
+					gs.nav_hunk("prev")
+				end, "Prev hunk")
+
+				-- Hunk actions
+				map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+				map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+				map("v", "<leader>hs", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, "Stage hunk")
+				map("v", "<leader>hr", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, "Reset hunk")
+
+				-- Buffer actions
+				map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
+				map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
+				map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
+				map("n", "<leader>hd", gs.diffthis, "Diff this")
+
+				-- Text object
+				map({ "o", "x" }, "ih", gs.select_hunk, "Select hunk")
+			end,
+		},
+	},
 
 	-- LazyGit
 	{
@@ -91,7 +133,41 @@ require("lazy").setup({
 			vim.keymap.set("n", "<C-p>", builtin.find_files)
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep)
 			vim.keymap.set("n", "<leader>fb", builtin.buffers)
+			vim.keymap.set("n", "<leader>fs", builtin.grep_string)
 		end,
+	},
+
+	-- Minimap
+	{
+		"Isrothy/neominimap.nvim",
+		version = "v3.x.x",
+		lazy = false,
+		keys = {
+			{ "<leader>nm", "<cmd>Neominimap Toggle<cr>", desc = "Toggle minimap" },
+			{ "<leader>nf", "<cmd>Neominimap Focus<cr>", desc = "Focus minimap" },
+			{ "<leader>nu", "<cmd>Neominimap Unfocus<cr>", desc = "Unfocus minimap" },
+		},
+		init = function()
+			vim.opt.wrap = false
+			vim.opt.sidescrolloff = 36
+			vim.g.neominimap = {
+				auto_enable = true,
+			}
+		end,
+	},
+
+	-- auto-session
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+
+		---enables autocomplete for opts
+		---@module "auto-session"
+		---@type AutoSession.Config
+		opts = {
+			suppressed_dirs = { "~/", "~/Downloads", "/" },
+			-- log_level = 'debug',
+		},
 	},
 
 	-- LSP
