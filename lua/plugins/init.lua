5c2d1abe3e5a873ef7a291bc48c38d181ce98c1b
Danny Harding <dannyharding10@gmail.com>
2026-02-13 09:41:07 -0600

    feat: add Primeagen's 99

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -189,6 +189,42 @@ require("lazy").setup({
 		},
 	},
 
+	-- 99 - AI Agent
+	{
+		"ThePrimeagen/99",
+		config = function()
+			local _99 = require("99")
+			local cwd = vim.uv.cwd()
+			local basename = vim.fs.basename(cwd)
+			_99.setup({
+				logger = {
+					level = _99.DEBUG,
+					path = "/tmp/" .. basename .. ".99.debug",
+					print_on_error = true,
+				},
+				completion = {
+					custom_rules = {},
+					files = {
+						max_file_size = 102400,
+						max_files = 5000,
+					},
+					source = "cmp",
+				},
+				md_files = {
+					"AGENT.md",
+				},
+			})
+
+			vim.keymap.set("v", "<leader>9v", function()
+				_99.visual()
+			end, { desc = "99 visual selection" })
+
+			vim.keymap.set("v", "<leader>9s", function()
+				_99.stop_all_requests()
+			end, { desc = "99 stop all requests" })
+		end,
+	},
+
 	-- LSP
 	{ import = "plugins.lsp" },
 
