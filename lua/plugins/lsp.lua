c4608d19e109b368cbe3d6f5a0d05e7588c3359a
Danny Harding <dannyharding10@gmail.com>
2026-02-13 11:21:40 -0600

    feat: add gh for opening diagnistics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,6 +30,9 @@ return {
 					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gh", function()
+						vim.diagnostic.open_float(nil, { scope = "cursor" })
+					end, opts)
 				end,
 			})
 
