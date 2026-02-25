169ec9a74053ea162a25e8f7377d3df223c669cf
Danny Harding <dannyharding10@gmail.com>
2026-02-25 10:13:27 -0600

    feat(keymaps.lua): add more keymaps
    add <Tab> and <S-Tab> for buffer navigation
    add <leader>b and <leader>fb for finding open buffers
    add <leader>cn and <leader>cp for navigating quickfix entries

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,6 +8,27 @@ map("i", "kj", "<Esc>")
 map("n", "<leader>w", "<cmd>w<cr>")
 map("n", "<leader>q", "<cmd>q<cr>")
 
+-- buffer navigation
+map("n", "<Tab>", "<cmd>bnext<cr>")
+map("n", "<S-Tab>", "<cmd>bprev<cr>")
+
+-- telescope buffers (MRU sort)
+local BUFFERS_DESC = "Find buffers (MRU)"
+local function findBuffers()
+	require("telescope.builtin").buffers({
+		sort_mru = true,
+		ignore_current_buffer = true,
+	})
+end
+
+map("n", "<leader>b", findBuffers, { desc = BUFFERS_DESC })
+map("n", "<leader>fb", findBuffers, { desc = BUFFERS_DESC })
+
+-- telescope oldfiles
+map("n", "<leader>fo", function()
+	require("telescope.builtin").oldfiles()
+end, { desc = "Find oldfiles" })
+
 -- window navigation
 map("n", "<C-h>", "<C-w>h")
 map("n", "<C-j>", "<C-w>j")
@@ -16,6 +37,10 @@ map("n", "<C-l>", "<C-w>l")
 
 -- turn off highlights
 map("n", "<leader>noh", "<cmd>noh<cr>")
+
+-- quickfix navigation
+map("n", "<leader>cn", "<cmd>cn<cr>", { desc = "Next quickfix" })
+map("n", "<leader>cp", "<cmd>cp<cr>", { desc = "Previous quickfix" })
 
 -- jump to definition in new buffer
 map("n", "gv", ":rightbelow vsplit | lua vim.lsp.buf.definition()<CR>")
